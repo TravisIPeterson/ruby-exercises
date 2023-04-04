55ecce8e51210b6d8f9ce12a9a237629055371a2
TravisIPeterson <travisianpeterson@gmail.com>
2023-04-03 19:55:03 -0700

    Add downcase check to greater than 0 count logic

--- substrings/substrings.rb
+++ substrings/substrings.rb
@@ -4,7 +4,7 @@ def substrings(string, dictionary)
     word_count = Hash.new(0)
     dictionary = dictionary.join(' ').downcase.split(' ')
     dictionary.each do |word|
-        if string.scan(word).count > 0
+        if string.downcase.scan(word).count > 0
             word_count[word] = string.downcase.scan(word).count
         end
     end
