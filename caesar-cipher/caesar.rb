f83250e789c444a6e04006d26aec3ac5e631154d
TravisIPeterson <travisianpeterson@gmail.com>
2023-04-03 17:20:50 -0700

    Complete cipher

--- caesar-cipher/caesar.rb
+++ caesar-cipher/caesar.rb
@@ -4,10 +4,18 @@ def caesar_cipher(string, int)
     cipher_array = []
     while i < string.length
         ascii_string[i] += int
+        if ascii_string[i] == 32 + int
+            ascii_string[i] -= int
+        elsif ascii_string[i] > 90 && ascii_string[i] < 91 + int
+            ascii_string[i] -= 26
+        elsif ascii_string[i] > 122
+            ascii_string[i] -= 26
+        end
         cipher_array[i] = ascii_string[i].chr
         i += 1
     end
     cipher_string = cipher_array.join('')
 end
 
-print caesar_cipher('What a string!', 5)
+puts caesar_cipher('What a string!', 5)
+puts caesar_cipher('ThIs Is a L0nGeR T3st!!!', 23)
